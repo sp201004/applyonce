26a7529758d38fb1a508555b32d7a99f3461d468
Surya Pratap Singh <sp01042002+github@gmail.com>
2026-08-18 16:07:07 +0530

    Enable production extension connect: add applyonce1.vercel.app to externally_connectable + WEB_APP_URL (localhost retained)

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 (function() {
   self.JOB_AUTOFILL_CONFIG = {
     BACKGROUND_BUILD_VERSION: 4,
-    WEB_APP_URL: "http://localhost:3000"
+    WEB_APP_URL: "https://applyonce1.vercel.app"
   };
 })();
